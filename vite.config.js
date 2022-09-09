e30dba6ff84a18a99c34c30457e8c25563687c48
ROYIANS <1294686101@qq.com>
2022-09-09 18:43:52 +0800

    🔧fix: 修复pages发布问题（只能这样了？）

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import vue2 from "@vitejs/plugin-vue2";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "./",
+  base: "/print-template-designer/",
   plugins: [
     vue2(),
     legacy({
