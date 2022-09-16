5f71d8a2b6730620abf92719be56c1efebe6ae11
ROYIANS <1294686101@qq.com>
2022-09-16 18:09:38 +0800

    📏feat: 添加标尺，虽然没什么用，但是好看

--- src/components/ruler/Ruler.vue
+++ src/components/ruler/Ruler.vue
@@ -5,11 +5,26 @@
 * @date 2022/9/16 11:42
 !-->
 <template>
-  <el-main class="Ruler"> 1 </el-main>
+  <div class="roy-ruler-box">
+    <div class="roy-ruler-outer-box"></div>
+    <div class="roy-ruler-horizontal-wrapper">
+      <RulerHorizontal />
+    </div>
+    <div class="roy-ruler-vertical-wrapper">
+      <RulerVertical />
+    </div>
+    <div class="roy-ruler-inner-box">
+      <div class="roy-ruler-inner-box-content">
+        <slot></slot>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
 import commonMixin from "@/mixin/commonMixin";
+import RulerHorizontal from "@/components/ruler/RulerHorizontal.vue";
+import RulerVertical from "@/components/ruler/RulerVertical.vue";
 
 /**
  * 标尺工具
@@ -17,7 +32,10 @@ import commonMixin from "@/mixin/commonMixin";
 export default {
   name: "RoyRuler",
   mixins: [commonMixin],
-  components: {},
+  components: {
+    RulerHorizontal,
+    RulerVertical,
+  },
   props: {},
   data() {
     return {};
