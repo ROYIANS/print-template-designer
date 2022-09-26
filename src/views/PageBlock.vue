eb3105c790557f09cc30244ccc26f5cc17e315d6
ROYIANS <1294686101@qq.com>
2022-09-26 15:29:46 +0800

    ✌feat: 页面和区块左侧面板UI

--- src/views/PageBlock.vue
+++ src/views/PageBlock.vue
@@ -1,56 +1,32 @@
 <template>
   <div class="roy-page-block">
-    <div>1</div>
-    <div>123456</div>
-    <div>123456</div>
-    <div>123456</div>
-    <div>123456</div>
-    <div>123456</div>
-    <div>123456</div>
-    <div>123456</div>
-    <div>123456</div>
-    <div>123456</div>
-    <div>123456</div>
-    <div>123456</div>
-    <div>123456</div>
-    <div>123456</div>
-    <div>123456</div>
-    <div>123456</div>
-    <div>123456</div>
-    <div>123456</div>
-    <div>123456</div>
-    <div>123456</div>
-    <div>123456</div>
-    <div>123456</div>
-    <div>123456</div>
-    <div>123456</div>
-    <div>123456</div>
-    <div>123456</div>
-    <div>123456</div>
-    <div>123456</div>
-    <div>123456</div>
-    <div>123456</div>
-    <div>123456</div>
-    <div>123456</div>
-    <div>123456</div>
-    <div>123456</div>
-    <div>123456</div>
-    <div>123456</div>
-    <div>123456</div>
-    <div>123456</div>
-    <div>123456</div>
-    <div>123456</div>
-    <div>123456</div>
-    <div>123456</div>
-    <div>123456</div>
-    <div>123456</div>
-    <div>123456</div>
+    <section v-for="(block, index) in blocks" :key="index">
+      <el-col
+        v-for="(span, bIndex) in block"
+        :key="bIndex"
+        :span="span"
+      ></el-col>
+    </section>
   </div>
 </template>
 
 <script>
 export default {
   name: "PageBlock",
+  data() {
+    return {
+      blocks: [
+        [24],
+        [12, 12],
+        [8, 8, 8],
+        [6, 6, 6, 6],
+        [8, 16],
+        [16, 8],
+        [4, 8, 4, 8],
+        [8, 4, 8, 4],
+      ],
+    };
+  },
 };
 </script>
 
@@ -59,5 +35,24 @@ export default {
   overflow: auto;
   height: calc(100% - 16px);
   padding: 8px;
+  section {
+    padding: 0;
+    background: var(--roy-bg-block-color);
+    height: 50px;
+    width: 100%;
+    user-select: none;
+    margin: 5px 0;
+    &:hover {
+      cursor: grab;
+      box-shadow: var(--roy-box-shadow-light);
+    }
+    .el-col {
+      height: 100%;
+      border: 2px solid var(--roy-border-block-color);
+      & + .el-col {
+        border-left: none;
+      }
+    }
+  }
 }
 </style>
