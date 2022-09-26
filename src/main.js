7dbc571b99c0386e69dd1bd8136815d5470cbf51
ROYIANS <1294686101@qq.com>
2022-09-26 17:37:36 +0800

    🌳feat: 左侧大纲树

--- src/main.js
+++ src/main.js
@@ -4,15 +4,18 @@ import { createPinia, PiniaVuePlugin } from "pinia";
 import App from "./App.vue";
 import Element from "element-ui";
 import router from "./router";
+import { VTree } from "vue-tree-halower2";
 
 import "./assets/main.scss";
 import "normalize.css/normalize.css";
 import "animate.css";
 import "./assets/theme/index.css";
 import "remixicon/fonts/remixicon.css";
+import "vue-tree-halower2/dist/halower-tree.min.css"; // 你可以自定义树的样式
 
 Vue.use(PiniaVuePlugin);
 Vue.use(Element);
+Vue.use(VTree);
 
 new Vue({
   router,
