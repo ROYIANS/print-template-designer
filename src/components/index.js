7cbd560bab1cd165ac0caaf4f940a4e2b49fc753
ROYIANS <1294686101@qq.com>
2022-11-28 15:39:11 +0800

    fix(0.1.2): text的HTML造成卡顿问题

--- src/components/index.js
+++ src/components/index.js
@@ -44,7 +44,7 @@ const install = function (Vue, opts = {}) {
   })
 }
 const Api = {
-  version: 'ROY-PRINT-DESIGNER@0.1.1',
+  version: 'ROY-PRINT-DESIGNER@0.1.2',
   PtdDesigner,
   PtdViewer,
   NightModeStore,
