341f7c0247a50dabb115c88f7afbb0503d2c2cb2
ROYIANS <1294686101@qq.com>
2023-08-25 18:04:03 +0800

    chore: change line endings to LF???

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,4 @@
 module.exports = {
   presets: ['@vue/cli-plugin-babel/preset'],
-  plugins: [
-    '@babel/plugin-proposal-object-rest-spread',
-    '@babel/plugin-proposal-optional-chaining'
-  ]
+  plugins: ['@babel/plugin-proposal-object-rest-spread', '@babel/plugin-proposal-optional-chaining']
 }
